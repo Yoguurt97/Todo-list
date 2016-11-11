7256906b529c023e0ebaca4628d5543dd738838a
Yoguurt97 <yoguurt97@mail.com>
2016-11-11 19:07:20 +1000

    add top menu

--- main.py
+++ main.py
@@ -5,28 +5,59 @@ from ctypes  import *
 
 # TODO Один файл в неделю/месяц
 # TODO Доработка GUI, сделать более приятный дизайн.
+# TODO Дописать удаление задач, инфу о программе(версия, хелпа)
 
+# Получение разешения экрана пользователя
 width = windll.user32.GetSystemMetrics(0) / 4 - 50
 height = windll.user32.GetSystemMetrics(1) / 4 - 50
-
+# Создание окна
 root = Tk()
 root.geometry('700x500+{0}+{1}'.format(int(width), int(height)))
 root.title('Todo list')
 root['bg'] = 'white'
 
+
+def m_delete_task():
+    win_delete = Tk()
+    win_delete.geometry('200x80+{0}+{1}'.format(int(width), int(height)))
+    area = Label(win_delete, text='123')
+    value = Entry(win_delete)
+    def delete_task():
+        date_task = value.get()
+        try:
+            with open('tasks\\{}.txt'.format(date_task), 'r') as f:
+                print('Задача удалена')
+        except:
+            pass
+    btn_delete = Button(win_delete, text='Удалить задачу', command=delete_task).pack(side='bottom')
+    value.pack(side='bottom')
+    area.pack(side='top')
+    win_delete.mainloop()
+
+# Создание топ меню
+m = Menu(root)
+root.config(menu=m)
+fm = Menu(m)
+m.add_cascade(label="Fie", menu=fm)
+fm.add_command(label="Delete task", command=m_delete_task)
+fm.add_command(label="Exit", command=root.destroy)
+hm = Menu(m)
+m.add_cascade(label="Help", menu=hm)
+hm.add_command(label="Help")
+hm.add_command(label="About")
+# Получение даты этого и следующего дня
 date = datetime.today()
 today = '{0}.{1}.{2}'.format(date.year, date.month, date.day )
 tommorow = '{0}.{1}.{2}'.format(date.year, date.month, date.day + 1)
-
+# Проверка на задачи
 def task_today(today, root):
     try:
         i = 1
-        with open('tasks\\' + today + '.txt', encoding='UTF-8') as f:
+        with open('tasks\\{}.txt'.format(today), encoding='UTF-8') as f:
             f_read = f.readlines()
             for line in f_read:
                 task = str(i) + '. ' + line
-                i = int(i)
-                i += 1
+                i = int(i) + 1
                 lab = Label(root, font='Airal 12', bg='white', text=task)
                 lab.pack()
         new_task(today, width, height)
@@ -34,6 +65,7 @@ def task_today(today, root):
         lab = Label(root, font='Airal 14', text='Задач на сегодня нет\n\n')
         lab.pack()
         new_task(today, width, height)
+# Новая задача
 def new_task(today, width, height):
     text_task = Text(root, height=7, width=300, font='Arial 14', wrap=WORD)
     text_task.pack()
@@ -51,11 +83,11 @@ def new_task(today, width, height):
             error_win.mainloop()
         else:
             try:
-                with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
+                with open('tasks\\{}.txt'.format(tommorow), 'a', encoding='UTF-8') as f:
                     f.write(task)
             except FileNotFoundError:
                 os.mkdir('tasks')
-                with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
+                with open('tasks\\{}.txt'.format(tommorow), 'a', encoding='UTF-8') as f:
                     f.write(task)
             task_add = Tk()
             task_add.geometry('100x100+{0}+{1}'.format(int(width), int(height)))
