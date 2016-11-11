87cfcacdede3434d0215aa9d9e6769008dd262a9
Yoguurt97 <yoguurt97@mail.com>
2016-11-11 15:35:04 +1000

    first commit

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
-import datetime
 import os
+from datetime import datetime
 from tkinter import *
 from ctypes  import *
 
 # TODO Один файл в неделю/месяц
-# TODO Доработка GUI, сделать более приятный дизайн, добавить оповещение о добавленной задачи, убрать {} в задачах на сегодня.
+# TODO Доработка GUI, сделать более приятный дизайн.
 
 width = windll.user32.GetSystemMetrics(0) / 4 - 50
 height = windll.user32.GetSystemMetrics(1) / 4 - 50
-width = int(width)
-height = int(height)
 
 root = Tk()
-root.geometry('700x500+{0}+{1}'.format(width, height))
+root.geometry('700x500+{0}+{1}'.format(int(width), int(height)))
 root.title('Todo list')
 root['bg'] = 'white'
 
+date = datetime.today()
+today = '{0}.{1}.{2}'.format(date.year, date.month, date.day )
+tommorow = '{0}.{1}.{2}'.format(date.year, date.month, date.day + 1)
 
-list_tasks = []
-
-date = datetime.datetime.today()
-today = str(date.year) + '.' + str(date.month) + '.' + str(date.day)
-next_day = date.day + 1
-tommorow = str(date.year) + '.' + str(date.month) + '.' + str(next_day)
-
-
-def task_today(today, list_tasks, root):
-    i = 1
+def task_today(today, root):
     try:
-        with open('tasks\\' + today + '.txt', 'r', encoding='UTF-8') as f:
+        i = 1
+        with open('tasks\\' + today + '.txt', encoding='UTF-8') as f:
             f_read = f.readlines()
-            print('Задачи на сегодня ' + today + ':')
             for line in f_read:
-                i = str(i)
-                print(i + '. ' + line)
-                task = i + '. ' + line
-                list_tasks.append(task)
+                task = str(i) + '. ' + line
                 i = int(i)
                 i += 1
                 lab = Label(root, font='Airal 12', bg='white', text=task)
@@ -44,34 +33,42 @@ def task_today(today, list_tasks, root):
     except FileNotFoundError:
         lab = Label(root, font='Airal 14', text='Задач на сегодня нет\n\n')
         lab.pack()
-        print('Задач на сегодня нет\n\n')
         new_task(today, width, height)
-
 def new_task(today, width, height):
     text_task = Text(root, height=7, width=300, font='Arial 14', wrap=WORD)
     text_task.pack()
     def add_task():
         task = text_task.get('1.0', END)
-        try:
-            with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
-                f.write(task)
-        except FileNotFoundError:
-            os.mkdir('tasks')
-            with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
-                f.write(task)
-        task_add = Tk()
-        task_add.geometry('100x100+{0}+{1}'.format(width, height))
-        task_add.title('Ok')
-        added = Label(task_add, text='Задача добавлена')
-        ok_added = Button(task_add, text='Оk', command=task_add.destroy)
-        text_task.delete('1.0', END)
-        ok_added.pack()
-        added.pack()
-        task_add.mainloop()
-
+        if task == '\n':
+            error_win = Tk()
+            error_win.geometry('100x100+{0}+{1}'.format(int(width), int(height)))
+            error_win.title('Ошибка')
+            error = Label(error_win, text='Задача\n не может быть пустой')
+            ok_added = Button(error_win, text='Оk', command=error_win.destroy)
+            text_task.delete('1.0', END)
+            ok_added.pack()
+            error.pack()
+            error_win.mainloop()
+        else:
+            try:
+                with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
+                    f.write(task)
+            except FileNotFoundError:
+                os.mkdir('tasks')
+                with open('tasks\\' + tommorow + '.txt', 'a', encoding='UTF-8') as f:
+                    f.write(task)
+            task_add = Tk()
+            task_add.geometry('100x100+{0}+{1}'.format(int(width), int(height)))
+            task_add.title('Ok')
+            added = Label(task_add, text='Задача добавлена')
+            ok_added = Button(task_add, text='Оk', command=task_add.destroy)
+            text_task.delete('1.0', END)
+            ok_added.pack()
+            added.pack()
+            task_add.mainloop()
     btn = Button(root, text='Добавить задачу', command=add_task)
     btn.pack()
     root.mainloop()
 
-task_today(today, list_tasks, root)
+task_today(today, root)
 root.mainloop()
